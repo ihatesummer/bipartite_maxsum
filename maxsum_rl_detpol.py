8f4ad51154f94ae9b8d948e0844329e45ce0ce3c
ihatesummer <ggglatrell@gmail.com>
2022-01-12 21:31:34 +0900

    1

--- maxsum_rl_detpol.py
+++ maxsum_rl_detpol.py
@@ -1,13 +1,11 @@
 # %%
-from maxsum import check_validity, get_pairing_matrix_argmax, get_pairing_matrix_sign, reshape_to_flat, reshape_to_square
+from maxsum import check_validity, get_pairing_matrix_argmax, reshape_to_square
 from maxsum_ul import get_datasets, forward_pass, decompose_dataset, FILENAMES, N_DATASET, N_NODE, SEED_W
-from torch.distributions import Normal
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
-import os
 import itertools
 
 np.set_printoptions(precision=3)
@@ -15,13 +13,15 @@ np.set_printoptions(suppress=True)
 GAMMA = 1.0
 N_TRAIN = 9000
 N_TEST = 1
-N_EPISODE = 10000
+N_EPISODE = 5000
 MAX_TIMESTEP = 10
 FILENAMES["nn_weight_alpha"] = "weights_rl_alpha.h5"
 FILENAMES["nn_weight_rho"] = "weights_rl_rho.h5"
 
 
 def main():
+    print("CUDA available:", torch.cuda.is_available(),
+          torch.cuda.get_device_name(torch.cuda.current_device()))
     pos_bs, pos_user, w, alpha_star, rho_star = get_datasets(
         FILENAMES, N_DATASET, SEED_W, N_NODE, "geographic")
     dim_in = 2*N_NODE**2
@@ -32,9 +32,6 @@ def main():
     optim_alpha = optim.Adam(pi_alpha.parameters(), lr=learning_rate)
     optim_rho = optim.Adam(pi_rho.parameters(), lr=learning_rate)
 
-    loss_history = []
-    reward_history = []
-    solved_history = []
     loss_fn = nn.MSELoss(reduction='sum')
     for data_no in range(N_TRAIN):
         print(f"Train set {data_no} weights:\n{reshape_to_square(w[data_no], N_NODE)}")
@@ -50,6 +47,8 @@ def main():
         # init_alpha, init_rho = cheat_start(init_alpha, init_rho,
         #                                     w[data_no], n_cheat_step=10)
 
+        reward_history = np.zeros(N_EPISODE)
+        solved_history = np.zeros(N_EPISODE, dtype=bool)
         for epi_no in range(N_EPISODE):
             alpha = init_alpha
             rho = init_rho
@@ -119,26 +118,28 @@ def main():
             D_rl = get_pairing_matrix_argmax(
                 reshape_to_square(alpha.detach().numpy(), N_NODE),
                 reshape_to_square(rho.detach().numpy(), N_NODE), N_NODE)
-            solved_history.append((D_rl==D_mp).all())
-            loss_history.append(loss_sum_alpha+loss_sum_rho)
+            solved_history[epi_no] = (D_rl==D_mp).all()
             total_reward_alpha = sum(pi_alpha.rewards)
             total_reward_rho = sum(pi_rho.rewards)
-            reward_history.append(total_reward_alpha + total_reward_rho)
+            reward_history[epi_no] = total_reward_alpha + total_reward_rho
 
             if epi_no % 1000 == 0:
                 print(f"(Dataset{data_no} episode{epi_no}) "
                     f"total_reward: {total_reward_alpha+total_reward_rho:.2f}, "
-                    f"sum_log_prob: {np.sum(pi_alpha.log_probs) + np.sum(pi_rho.log_probs):.2f}, "
                     f"loss: {loss_sum_alpha+loss_sum_rho:.2f}, "
                     f"D_rl==D_mp: {(D_rl==D_mp).all()}")
-                compare_with_target(alpha_hist, alpha_target_hist, rho_hist, rho_target_hist,
-                                    data_no, epi_no)
+                # compare_with_target(alpha_hist, alpha_target_hist, rho_hist, rho_target_hist,
+                #                     data_no, epi_no)
+                np.save(f"training/ds{data_no}_epi{epi_no}_alpha_hist.npy", alpha_hist)
+                np.save(f"training/ds{data_no}_epi{epi_no}_alpha_target_hist.npy", alpha_target_hist)
+                np.save(f"training/ds{data_no}_epi{epi_no}_rho_hist.npy", rho_hist)
+                np.save(f"training/ds{data_no}_epi{epi_no}_rho_target_hist.npy", rho_target_hist)
             pi_alpha.onpolicy_reset()
             pi_rho.onpolicy_reset()
+        if data_no % 1000 == 0:
+            plot_reward(reward_history, solved_history, data_no)
     torch.save(pi_alpha.model.state_dict(), FILENAMES["nn_weight_alpha"])
     torch.save(pi_rho.model.state_dict(), FILENAMES["nn_weight_rho"])
-    plot_reward_loss_vs_epoch(
-        reward_history, loss_history, solved_history, data_no)
 
 
 class Pi(nn.Module):
@@ -159,7 +160,6 @@ class Pi(nn.Module):
         self.train()  # set training mode
 
     def onpolicy_reset(self):
-        self.log_probs=[]
         self.rewards=[]
 
     def act(self, state):
@@ -213,38 +213,22 @@ def plot_positions(bs, user, idx):
     plt.close('all')
 
 
-def plot_reward_loss_vs_epoch(reward_history,
-                              loss_history,
+def plot_reward(reward_history,
                               solved_history, idx):
-    _, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
-    axes[0].set_title('reward')
-    axes[1].set_title('loss')
+    _, ax = plt.subplots()
+    ax.set_title('reward')
     epi = np.linspace(0, len(reward_history)-1, len(reward_history))
 
     reward_history_solved = np.asfarray(reward_history)
     reward_history_solved[[not solved for solved in solved_history]] = np.nan
     reward_history_unsolved = np.asfarray(reward_history)
     reward_history_unsolved[solved_history] = np.nan
-    axes[0].plot(epi, reward_history_solved,
-                 "*", color='green', markersize=1)
-    axes[0].plot(epi, reward_history_unsolved,
-                 "o", color='red', markersize=1)
-    axes[0].plot(epi, reward_history,
-                 "-", color='black', alpha=0.2)
-    axes[0].set_xlim(xmin=0)
-
-    loss_history_solved = np.asfarray(loss_history)
-    loss_history_solved[[not solved for solved in solved_history]]=np.nan
-    loss_history_unsolved=np.asfarray(loss_history)
-    loss_history_unsolved[solved_history]=np.nan
-    axes[1].plot(epi, loss_history_solved,
-                 "o", color='green', markersize=2)
-    axes[1].plot(epi, loss_history_unsolved,
-                 "o", color='red', markersize=2)
-    axes[1].plot(epi, loss_history,
-                 "-", color='black', alpha=0.2)
-    axes[1].set_xlim(xmin=0)
-    plt.savefig(f"training/dataset{idx}.png")
+    ax.plot(epi, reward_history,
+                 "-", color='black', alpha=0.5)
+    ax.plot(epi, reward_history_solved,
+                 "*", color='green', markersize=3)
+    ax.set_xlim(xmin=0)
+    plt.savefig(f"training/dataset{idx}_reward.png")
     plt.close('all')
 
 
@@ -273,8 +257,6 @@ def compare_with_target(alpha_hist, alpha_target_hist, rho_hist, rho_target_hist
         axes[i, 1].set_xlim(xmin=0, xmax=MAX_TIMESTEP-1)
         # axes[i, 0].set_ylim(ymin=-3, ymax=3)
         # axes[i, 1].set_ylim(ymin=-3, ymax=3)
-    # axes[0].legend()
-    # axes[1].legend()
     plt.savefig(f"training/dataset{idx}_epi{episode}.png")
     plt.close('all')
 
